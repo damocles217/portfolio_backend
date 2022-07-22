5878b9736fce85004f9f78724d0a89b4961c7955
dam0cl3s <damm084@tutanota.com>
2022-07-21 20:33:51 -0500

    Added confs for cors

--- src/server.js
+++ src/server.js
@@ -7,7 +7,20 @@ const app = express();
 
 app.set("PORT", process.env.PORT || 8000);
 
-app.use((req, _, next) => {
+app.use((req, response, next) => {
+	response.setHeader(
+		"Access-Control-Allow-Origin",
+		process.env.CORS_OPTIONS || "*"
+	);
+	response.setHeader("Access-Control-Allow-Credentials", "true");
+	response.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET,HEAD,OPTIONS,POST,PUT"
+	);
+	response.setHeader(
+		"Access-Control-Allow-Headers",
+		"Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+	);
 	if (req.method == "OPTIONS") next();
 });
 app.use(express.json());
